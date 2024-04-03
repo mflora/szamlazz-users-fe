bfb95f7db07c17aa35a0e800f6c9c310eeff9235
Flóra <flosztya@yahoo.com>
2024-04-03 09:07:31 +0200

    creating routing and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {UserListComponent} from "../pages/user-list/user-list.component";
+import {CreateUserComponent} from "../pages/create-user/create-user.component";
+import {ModifyUserComponent} from "../pages/modify-user/modify-user.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: UserListComponent},
+  {path: 'create-user', component: CreateUserComponent},
+  {path: 'modify-user', component: ModifyUserComponent},
+];
---
+++ src/pages/modify-user/modify-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-user.component.html',
+  styleUrl: './modify-user.component.scss'
+})
+export class ModifyUserComponent {
+
+}
