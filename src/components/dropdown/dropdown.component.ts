91ecb11f0cd1f262934b61357a54d221f806e9fa
Flóra <flosztya@yahoo.com>
2024-04-06 14:41:56 +0200

    rewriting dropdown component

--- src/components/dropdown/dropdown.component.ts
+++ src/components/dropdown/dropdown.component.ts
@@ -1,13 +1,16 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    FaIconComponent
   ],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss'
@@ -24,6 +27,9 @@ export class DropdownComponent {
   @Input()
   control = new FormControl('');
 
+  isModalOpen = false;
+  selectedValueDisplay = '';
+
   selectionChange(value: string) {
     this.onSelectionChange.emit(value);
   }
@@ -35,5 +41,17 @@ export class DropdownComponent {
   set value(val: string) {
     this.control.setValue(val);
     this.onSelectionChange.emit(val);
+  }
+
+  protected readonly faChevronDown = faChevronDown;
+
+  toggleDropdown() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  setDropdownValue(option: { value: string; displayName: string }) {
+    this.selectedValueDisplay = option.displayName;
+    this.value = option.value;
+    this.isModalOpen = false;
   }
 }
