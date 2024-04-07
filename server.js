9abbd1058625ab107b99e264572341a971696224
Flóra <flosztya@yahoo.com>
2024-04-07 15:50:22 +0200

    heroku changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/users-fe'));
+app.use(express.static(__dirname + '/dist/users-fe/browser'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/users-fe/browser/index.html'));});
