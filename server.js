2df180b4cc3ca06989764c0fd17c616e1f4b9a5e
Flóra <flosztya@yahoo.com>
2024-04-07 15:42:44 +0200

    heroku changes

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/users-fe'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/users-fe/browser/index.html'));});
+    '/dist/users-fe/index.html'));});
 app.listen(process.env.PORT || 8080);
