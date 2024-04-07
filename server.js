3fad1f19ebbd9b1519ec07344a2a54f216736b10
Flóra <flosztya@yahoo.com>
2024-04-07 15:32:40 +0200

    heroku changes

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/<app-name>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(8080);
